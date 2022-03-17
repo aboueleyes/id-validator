79b8870423aeb8cd32e11b00d7997df8825d2cd4
Ibrahim Abou Elenein <ibrahimabouelanin@gmail.com>
2022-03-17 22:55:56 +0200

    Add views file
    implemnted the index function returing the json.

--- idVaildator/core/views.py
+++ idVaildator/core/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from django.http import HttpRequest, JsonResponse
+from .EgyptianNationalId import *
 
-# Create your views here.
+
+def index(request: HttpRequest, id: str) -> JsonResponse:
+    """
+    Handle the request to the index page.
+    Args:
+        request (HttpRequest): The request object.
+        id (str): The Natioal ID
+
+    Returns:
+        JsonResponse: The response object.
+    """
+    try:
+        id: EgyptianNationalId = EgyptianNationalId(id)
+    except:
+        return JsonResponse({'error': 'Invalid ID'}, status=400)
+    return JsonResponse(id.fields)
