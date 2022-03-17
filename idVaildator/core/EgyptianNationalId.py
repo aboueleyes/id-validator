734e83fcbd4ef6ceb7445ad65a77276c448dea67
Restyled.io <commits@restyled.io>
2022-03-17 22:12:34 +0000

    Restyled by reorder-python-imports

--- idVaildator/core/EgyptianNationalId.py
+++ idVaildator/core/EgyptianNationalId.py
@@ -1,5 +1,6 @@
 import re
-from datetime import datetime, timedelta
+from datetime import datetime
+from datetime import timedelta
 
 EG_ID_REGEX: str = r"^(2|3)[0-9]{2}[0-1][0-9][0-3][0-9](01|02|03|04|11|12|13|14|15|16|17|18|19|21|22|23|24|25|26|27|28|29|31|32|33|34|35|88)\d{5}$"
 
