c62874ef7e84789cf59734ba2fac1c58c29dad7e
Restyled.io <commits@restyled.io>
2022-03-17 21:02:46 +0000

    Restyled by black

--- idVaildator/core/tests.py
+++ idVaildator/core/tests.py
@@ -2,7 +2,7 @@ import unittest
 from .EgyptianNationalId import EgyptianNationalId
 import datetime
 
-VALID_ID = '30103211203135'
+VALID_ID = "30103211203135"
 
 
 class TestNationalId(unittest.TestCase):
@@ -10,10 +10,10 @@ class TestNationalId(unittest.TestCase):
         self.id = EgyptianNationalId(VALID_ID)
 
     def test_validaty(self):
-        id_2 = 'abc123'
-        id_3 = '301031120135'
-        id_4 = '4010311203135'
-        id_5 = '30103214403135'
+        id_2 = "abc123"
+        id_3 = "301031120135"
+        id_4 = "4010311203135"
+        id_5 = "30103214403135"
 
         with self.assertRaises(ValueError):
             EgyptianNationalId(id_2)
@@ -26,20 +26,21 @@ class TestNationalId(unittest.TestCase):
         self.assertEqual(self.id.is_valid(), True)
 
     def test_gender(self):
-        id_female = EgyptianNationalId('29501023201922')
-        self.assertEqual(self.id.fields['gender'], 'Male')
-        self.assertEqual(id_female.fields['gender'], 'Female')
+        id_female = EgyptianNationalId("29501023201922")
+        self.assertEqual(self.id.fields["gender"], "Male")
+        self.assertEqual(id_female.fields["gender"], "Female")
 
     def test_governarate(self):
-        self.assertEqual(self.id.fields['governrate'], 'Dakahlia')
+        self.assertEqual(self.id.fields["governrate"], "Dakahlia")
 
     def test_birthday(self):
-        self.assertEqual(self.id.fields['birthDate'],
-                         datetime.datetime(2001, 3, 21).date())
+        self.assertEqual(
+            self.id.fields["birthDate"], datetime.datetime(2001, 3, 21).date()
+        )
 
     def test_century(self):
         self.assertTrue(2002 in self.id.century)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()
